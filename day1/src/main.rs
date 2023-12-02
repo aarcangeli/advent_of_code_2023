64cbc81bf2ac88b9306ee1c65b28288974a077a6
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2023-12-02 01:42:15 +0100

    add part two

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -15,74 +15,55 @@ fn main() {
     }
 }
 
-fn part_one(file: String) {
+fn part_two(file: String) {
     let mut full_sum = 0;
+
+    let numbers = vec![
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ];
 
     for line in file.lines() {
         let mut first_digit: i32 = -1;
         let mut last_digit: i32 = -1;
 
-        for char in line.chars() {
-            match char.to_string().parse::<i32>() {
-                Ok(num) => {
-                    first_digit = num;
-                    break;
+        let mut first_digit_position: Option<usize> = None;
+        let mut last_digit_position: Option<usize> = None;
+
+        for (as_string, value) in numbers.iter() {
+            match line.find(as_string) {
+                None => {}
+                Some(position) => {
+                    if !first_digit_position.is_some() || position < first_digit_position.unwrap() {
+                        first_digit_position = Some(position);
+                        first_digit = value.clone();
+                    }
                 }
-                Err(_) => {}
             }
-        }
-
-        for char in line.chars() {
-            match char.to_string().parse::<i32>() {
-                Ok(num) => {
-                    last_digit = num;
+            match line.rfind(as_string) {
+                None => {}
+                Some(position) => {
+                    if !last_digit_position.is_some() || position > last_digit_position.unwrap() {
+                        last_digit_position = Some(position);
+                        last_digit = value.clone();
+                    }
                 }
-                Err(_) => {}
-            }
-        }
-
-        let full_num = first_digit * 10 + last_digit;
-        full_sum = full_sum + full_num;
-        // println!("Line: {full_num}");
-    }
-
-    println!("Result: {full_sum}")
-}
-
-fn part_two(file: String) {
-    let mut full_sum = 0;
-
-    for mut line in file.lines() {
-        let mut first_digit: i32 = -1;
-        let mut last_digit: i32 = -1;
-
-        let line2 = line
-            .replace("one", "1")
-            .replace("two", "2")
-            .replace("three", "3")
-            .replace("four", "4")
-            .replace("five", "5")
-            .replace("six", "6")
-            .replace("seven", "7")
-            .replace("eight", "8")
-            .replace("nine", "9");
-
-        for char in line2.chars() {
-            match char.to_string().parse::<i32>() {
-                Ok(num) => {
-                    first_digit = num;
-                    break;
-                }
-                Err(_) => {}
-            }
-        }
-
-        for char in line2.chars() {
-            match char.to_string().parse::<i32>() {
-                Ok(num) => {
-                    last_digit = num;
-                }
-                Err(_) => {}
             }
         }
 
